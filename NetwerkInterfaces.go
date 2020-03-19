1ca0e86803136e3bf5d2c6302e9d8ce9c27e224b
marijnvalks <62105714+marijnvalks@users.noreply.github.com>
2020-03-19 11:07:29 +0100

    Add Exercise
    Eerste opdracht in Go, simpel programma dat je netwerk kaarten uitleest. Gebruik gemaakt van "net" libary.

---
+++ NetwerkInterfaces.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"strings"
+)
+
+func main() {
+	fmt.Println("---- Network Interface Finder by Marijn  ----")
+
+	ifaces, _ := net.Interfaces()
+	for _, iface := range ifaces {
+		fmt.Println("\nNetwork Interface:", iface)
+
+		addrs, _ := iface.Addrs()
+		for _, addr := range addrs {
+			addrStr := addr.String()
+			fmt.Println("    Network Adres: ", addr.Network(), addrStr)
+
+			// Must drop the stuff after the slash in order to convert it to an IP instance
+			split := strings.Split(addrStr, "/")
+			networkadres := split[0]
+
+			// Parse the string to an IP instance
+			ip := net.ParseIP(networkadres)
+			if ip.To4() != nil {
+				fmt.Println("       ", networkadres, "is a ipv4 adres.")
+			} else {
+				fmt.Println("       ", networkadres, "is a ipv6 adres.")
+			}
+			fmt.Println("       ", networkadres, "is interface-local multicast :", ip.IsInterfaceLocalMulticast())
+			fmt.Println("       ", networkadres, "is link-local multicast      :", ip.IsLinkLocalMulticast())
+			fmt.Println("       ", networkadres, "is link-local unicast        :", ip.IsLinkLocalUnicast())
+			fmt.Println("       ", networkadres, "is global unicast            :", ip.IsGlobalUnicast())
+			fmt.Println("       ", networkadres, "is multicast                 :", ip.IsMulticast())
+			fmt.Println("       ", networkadres, "is loopback                  :", ip.IsLoopback())
+		}
+	}
+
+}
